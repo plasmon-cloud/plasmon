f291e9ea44ee63d0a29dd9db5ea1f1185d406834
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-10 18:18:45 +0000

    Export native process runtime

---
+++ apps/plasmon/src/os/process/index.ts
@@ -0,0 +1,11 @@
+export { NativeApplicationRegistry } from "./registry.ts";
+export { NativeProcessController } from "./controller.ts";
+export type { NativeProcessControllerOptions } from "./controller.ts";
+export { NativeProcessHost } from "./NativeProcessHost.tsx";
+export type { NativeProcessHostProps } from "./NativeProcessHost.tsx";
+export type {
+  NativeAppComponent,
+  NativeAppComponentProps,
+  NativeAppLoader,
+  NativeAppModule,
+} from "./runtime.ts";
