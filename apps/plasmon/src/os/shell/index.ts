68c2a0d6f9d690435b26ac8c73dd58a5439b3c25
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-10 21:51:04 +0000

    shell: export search opening helper

--- apps/plasmon/src/os/shell/index.ts
+++ apps/plasmon/src/os/shell/index.ts
@@ -4,4 +4,5 @@ export * from "./calendar.ts";
 export * from "./model.ts";
 export * from "./preferences.ts";
 export * from "./search.ts";
+export * from "./searchOpening.ts";
 export * from "./subscriptions.ts";
