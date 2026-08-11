887a541e2efa8f3ab61a8840d3f90363c6765951
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-11 20:01:47 +0000

    Make GUI2 supersede the GUI experiment in place

--- apps/plasmon/src/index.tsx
+++ apps/plasmon/src/index.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { DesktopShell2 } from "./gui2/DesktopShell2.tsx";
+import { DesktopShell2 as DesktopShell } from "./desktop/DesktopShell.tsx";
 import { installAppIconFallbacks } from "./iconFallback.ts";
 import "./style.scss";
-import "./gui2/desktop2.scss";
+import "./desktop.scss";
+import "./desktop-overrides.scss";
 
 installAppIconFallbacks();
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
-createRoot(container).render(<DesktopShell2 />);
+createRoot(container).render(<DesktopShell />);
