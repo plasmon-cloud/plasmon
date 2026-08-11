c9856b4bbf3735c6a8edd31e690b4ff61c16683a
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-11 00:40:58 +0000

    Export FsService association default store

--- apps/plasmon/src/os/associations/index.ts
+++ apps/plasmon/src/os/associations/index.ts
@@ -1,6 +1,7 @@
 export * from "./atom.ts";
 export * from "./atomPackage.ts";
 export * from "./defaults.ts";
+export * from "./fsDefaults.ts";
 export * from "./openWith.ts";
 export * from "./registry.ts";
 export * from "./shortcut.ts";
