caa86cc7bdff18743ae3d1a53225999a46ba6efe
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-11 05:55:14 +0000

    export MTN authorization boundary

--- apps/plasmon/src/os/neutron/index.ts
+++ apps/plasmon/src/os/neutron/index.ts
@@ -2,5 +2,6 @@ export * from "./bridge.ts";
 export * from "./icon-resolver.ts";
 export * from "./lifecycle.ts";
 export * from "./mock.ts";
+export * from "./mtn-authorization.ts";
 export * from "./types.ts";
 export * from "./vanilla.ts";
