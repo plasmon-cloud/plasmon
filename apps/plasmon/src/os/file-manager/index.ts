b50e865c046e2e75374ce2e729da8411f3332f24
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-12 16:09:46 +0000

    Export FileManager Create Shortcut action

--- apps/plasmon/src/os/file-manager/index.ts
+++ apps/plasmon/src/os/file-manager/index.ts
@@ -1,5 +1,6 @@
 export * from "./model.ts";
 export * from "./activation.ts";
+export * from "./create-shortcut.ts";
 export * from "./delete.ts";
 export * from "./FileManager.tsx";
 export * from "./properties.tsx";
