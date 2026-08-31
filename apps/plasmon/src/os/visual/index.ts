1db72ea3c441e7a79e5a8056a784761c4673dbef
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-31 02:40:37 +0000

    #739 Export Visual configuration authority

--- apps/plasmon/src/os/visual/index.ts
+++ apps/plasmon/src/os/visual/index.ts
@@ -2,6 +2,7 @@ import "./visual.scss";
 import "./adaptive-surfaces.scss";
 
 export * from "./assets.ts";
+export * from "./configuration.ts";
 export * from "./presentation.ts";
 export * from "./resource-presentation.ts";
 export * from "./sizing.ts";
