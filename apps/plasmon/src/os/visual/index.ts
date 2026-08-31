d0837790d3b1c5d4f179b395e7b6c9fda0ea0089
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-31 02:35:43 +0000

    feat(personalization): export visual personalization semantics

--- apps/plasmon/src/os/visual/index.ts
+++ apps/plasmon/src/os/visual/index.ts
@@ -3,6 +3,7 @@ import "./adaptive-surfaces.scss";
 
 export * from "./assets.ts";
 export * from "./presentation.ts";
+export * from "./personalization.ts";
 export * from "./resource-presentation.ts";
 export * from "./sizing.ts";
 export * from "./primitives.tsx";
